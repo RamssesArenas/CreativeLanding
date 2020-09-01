f3c3db0ac058fbfb5e0431520c733c5e996ef2cc
RamssesArenas <69809004+RamssesArenas@users.noreply.github.com>
2020-09-01 01:16:45 -0500

    Add files via upload

---
+++ docs/precache-manifest.d15444f936931925d554e777babbd163.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ccf5b0339c54866ff6bc3d81acdb09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0884aadc13ab483fd06",
+    "url": "/static/css/main.4c426cc8.chunk.css"
+  },
+  {
+    "revision": "4e91b17df9941f712198",
+    "url": "/static/js/2.11807230.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.11807230.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0884aadc13ab483fd06",
+    "url": "/static/js/main.1fb3ea4a.chunk.js"
+  },
+  {
+    "revision": "275991a6a66a81924c04",
+    "url": "/static/js/runtime-main.de35829d.js"
+  },
+  {
+    "revision": "fc7d3155f65b3dbdae5a6a9faff93dd0",
+    "url": "/static/media/pepsi001.fc7d3155.png"
+  },
+  {
+    "revision": "671ffb98d7d965791a55949a584a96d1",
+    "url": "/static/media/pepsi002.671ffb98.png"
+  },
+  {
+    "revision": "6eeb577ec4105dfefee281723411e9e2",
+    "url": "/static/media/pepsi003.6eeb577e.png"
+  }
+]);
